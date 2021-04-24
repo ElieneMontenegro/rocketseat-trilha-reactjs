d33e09d538869cc9907f58a74e4c46855eb8ca89
ElieneMontenegro <lila.amaral@hotmail.com>
2021-04-24 07:05:32 -0300

    fetch episodes data to show on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,82 @@
-import Head from 'next/head'
-import { useEffect } from 'react'
-import { Header } from '../components/Header'
+import Head from "next/head";
+import { useEffect } from "react";
+import { Header } from "../components/Header";
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 
-export default function Home(props) {
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  thumbnail: string;
+  publishedAt: string;
+  duration: number;
+  durationAsString: string;
+  description: string;
+  url: string;
+};
+
+type HomeProps = {
+  episodes: Episode[];
+};
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
       <p>{JSON.stringify(props.episodes)}</p>
     </div>
-  )
+  );
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const episodes: Episode[] = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
 
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
-  }
-}
-
+  };
+};
 
 // formato SPA -> single page app - função dentro da home
-  //quando algo mudar no meu app, algo acontece:
-  /*useEffect(() => {
+//quando algo mudar no meu app, algo acontece:
+/*useEffect(() => {
     fetch('http://localhost:3333/episodes')
       .then(response => response.json())
       .then(data => console.log(data))
   }, []) */
-  // array vazio faz com que o useEffect dispare só uma vez
-  // quando o componente for exibido na tela
-  // ou seja, quando a pessoa acessa a tela, essa requisição é disparada e retorna os dados no console
-
+// array vazio faz com que o useEffect dispare só uma vez
+// quando o componente for exibido na tela
+// ou seja, quando a pessoa acessa a tela, essa requisição é disparada e retorna os dados no console
 
 // formato SSR -> server side rendering
 /*
@@ -53,7 +95,6 @@ export async function getServerSideProps() {
 // nesse formato, a requisição é feita na camada do next
 // além disso, o getServerSideProps executa toda vez que alguém acessar a home
 // mas se a home não sofre muitas alterações, não tem pq isso acontecer
-
 
 // SSG -> static site generation
 // gera uma versão estática da página para que não precise ficar recarregando a cada nova pessoa que acessa
